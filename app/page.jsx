a016e2869a5f0477ca26d669818170b2ff17da4c
Hammad <javedhammad034@gmail.com>
2024-06-20 12:23:25 -0700

    added form

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="content">
+      <div className="introduction">
+        Whether you're scheduling a doctor's appointment, organizing your closet, or finally fixing that squeaky door, our quirky ticket system turns each task into a VIP experience. Grab a ticket, take on the task, and revel in the joy of crossing it off with a flourish! 
+      </div>
+      <div className="introduction">
+        Whether you're scheduling a doctor's appointment, organizing your closet, or finally fixing that squeaky door, our quirky ticket system turns each task into a VIP experience. Grab a ticket, take on the task, and revel in the joy of crossing it off with a flourish! 
+      </div>
+      <div className="introduction">
+        Whether you're scheduling a doctor's appointment, organizing your closet, or finally fixing that squeaky door, our quirky ticket system turns each task into a VIP experience. Grab a ticket, take on the task, and revel in the joy of crossing it off with a flourish! 
+      </div>
       <div className="introduction">
         Whether you're scheduling a doctor's appointment, organizing your closet, or finally fixing that squeaky door, our quirky ticket system turns each task into a VIP experience. Grab a ticket, take on the task, and revel in the joy of crossing it off with a flourish! 
       </div>
--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -2,6 +2,39 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div className='content'>
+        <div class="form-container">
+            <h2 class="form-heading">New Ticket</h2>
+            <form action="">
+            <div className='formSection'>
+                    <label htmlFor="due">Due Date</label>
+                    <input type="date" id="due" name="due"/>
+            </div>
+            <div className='formSection'>
+                    <div>
+                        <p style={{margin:"0"}}>Priority</p>
+                        <div>
+                            <label htmlFor="high">
+                                High
+                               <input type="radio" id="high" name="priority" checked/>
+                            </label>
+                            <label htmlFor="med">
+                                Med
+                                <input type="radio" id="med" name="priority"/>
+                            </label>
+                            <label htmlFor="low">
+                                Low
+                                <input type="radio" id="low" name="priority"/>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div className='formSection desc'>
+                    <label htmlFor="description">Description</label>
+                    <textarea id="description" name="description" placeholder='Task Description'/>
+                </div>
+            </form>
+        </div>
+    </div>
   )
 }
