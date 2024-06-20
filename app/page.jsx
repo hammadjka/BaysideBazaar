05ed71c97a981dc06d5462c152e08ac93175118f
Hammad <javedhammad034@gmail.com>
2024-06-20 00:35:16 -0700

    added base UI, color scheme and structure

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <div className="introduction">
+        Whether you're scheduling a doctor's appointment, organizing your closet, or finally fixing that squeaky door, our quirky ticket system turns each task into a VIP experience. Grab a ticket, take on the task, and revel in the joy of crossing it off with a flourish! 
+      </div>
     </main>
   );
 }
