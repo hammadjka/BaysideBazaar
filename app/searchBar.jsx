fe14d1ada575369372504b94c05c3a94a27a2ba0
Hammad <javedhammad034@gmail.com>
2024-06-28 01:05:34 -0700

    fixed naming warnings

--- app/searchBar.jsx
+++ app/searchBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-export default function ({onSearch}) {
+export default function SearchBar ({onSearch}) {
     return (
         <div className="searchdiv" >
             <input onChange={onSearch} className="searchbar" type="text" placeholder='Search' />
